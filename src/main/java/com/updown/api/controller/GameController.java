22cf2df943cc1e44b8c0a0f0d6470e05b8f788a8
sc2bat <izar_dero@naver.com>
2023-12-27 11:50:34 +0900

    get api image test done

--- src/main/java/com/updown/api/controller/GameController.java
+++ src/main/java/com/updown/api/controller/GameController.java
@@ -1,9 +1,16 @@
 package com.updown.api.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +28,15 @@ public class GameController {
 	public List<Map<String, Object>> getGameInfo(){
 		return gameService.getGameInfo();
 	}
+
+	@GetMapping("/image/sample")
+	public ResponseEntity<byte[]> getImageSample() throws IOException{
+		Resource resource = new ClassPathResource("/static/images/sample.png");
+        byte[] imageBytes = Files.readAllBytes(Path.of(resource.getURI()));
+
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imageBytes);
+	}
+	
+	
 	
 }
